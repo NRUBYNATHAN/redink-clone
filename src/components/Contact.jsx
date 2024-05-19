4ef0646f469803cd4d4f4f3ec0ab6eb0b3dacf7b
RUBYNATHAN N <rubynathan999@gmail.com>
2024-05-19 18:55:18 +0530

    responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,23 @@
 function Contact() {
   return (
     <div className="flex justify-center items-center mx-20 mt-10">
-      <div className="w-full h-[500px] bg-cover bg-center rounded-xl bg-custom-gradient bg-no-repeat ">
+      <div className="w-full py-4 bg-cover bg-center rounded-xl bg-custom-gradient bg-no-repeat ">
         <div className="flex justify-around items-center">
-          <div className="w-[500px] m-20 ">
-            <p className="bg-white text-gray-400 p-4 rounded-[50px] mb-4">
+          <div className="w-[150px] m-0 mt-4 md:w-[500px] md:m-20 ">
+            <p className="bg-white text-[10px] md:text-[18px] text-gray-400 p-2 md:p-4 rounded-[50px] mb-4">
               Your Name
             </p>
-            <p className="bg-white text-gray-400 p-4 rounded-[50px] mb-4">
+            <p className="bg-white text-gray-400 text-[10px] md:text-[18px] p-2 md:p-4 rounded-[50px] mb-4">
               Name of Your Institute
             </p>
-            <p className="bg-white text-gray-400 p-4 rounded-[50px] mb-4">
+            <p className="bg-white text-gray-400 text-[10px] md:text-[18px] p-2 md:p-4 rounded-[50px] mb-4">
               Your Mobile Number
             </p>
-            <p className="bg-white text-gray-400 p-4 rounded-[50px] mb-4">
+            <p className="bg-white text-gray-400 text-[10px] md:text-[18px] p-2 md:p-4 rounded-[50px] mb-4">
               Your Role
             </p>
             <div className="flex justify-center items-center">
-              <p className="bg-blue-700 w-[200px] h-[50px] text-center rounded-[10px] text-[20px] font-bold text-white pt-2">
+              <p className="bg-blue-700 w-[100px] h-[40px] text-[15px] md:w-[200px] md:h-[50px] md:text-[20px] text-center rounded-[10px]  font-bold text-white pt-2">
                 Submit
               </p>
             </div>
